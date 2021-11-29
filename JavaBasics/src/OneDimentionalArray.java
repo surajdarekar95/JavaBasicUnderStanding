8f819448252d5d21c1252c22cca38d0a4bf3767c
surajdarekar95 <darekarsuraj10395@gmail.com>
2021-11-30 00:01:42 +0530

    One Dimentional Array

---
+++ JavaBasics/src/OneDimentionalArray.java
@@ -0,0 +1,30 @@
+
+public class OneDimentionalArray {
+
+	public static void main(String[] args) {
+
+		//How to creat String array
+		
+		/*String a[]= new String[4];
+		a[0]="suraj";
+		a[1]="darekar";
+		a[2]="from";
+		a[3]="satara";
+		
+		
+		for(int i=0;i<4;i++) {
+			System.out.print(" "+a[i]);
+		}*/
+		
+		//How to creat integer array
+		
+		int num[]= {10,20,30,40};
+
+		for(int i=0;i<4;i++) {
+			
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
